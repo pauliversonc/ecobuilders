414c2daa74f44f458817024844262e3c7c95a883
Paul Iverson Cortez <pauliversonc@gmail.com>
2021-06-15 17:08:20 +0800

    added landing page features

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,19 @@ module.exports = {
       fontFamily: {
         custom: ['Poppins']
       },
+      colors: {
+        lime: {
+          '100': '#b4d669',
+          '200': '#aad055',
+          '300': '#a0cb41',
+          '400': '#93be34',
+          '500': '#84aa2f',
+          '600': '#749629',
+          '700': '#658224',
+        }
+      },
       backgroundImage: theme => ({
-        'main-pic': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url('/images/webaliser-_TPTXZd9mOo-unsplash.jpg')",
+        'main-pic': "linear-gradient(rgba(0, 0, 0, 0.2), rgba(255, 255, 255, 0), rgba(0, 0, 0, 1)), url('/images/webaliser-_TPTXZd9mOo-unsplash.jpg')",
        })
     },
   },

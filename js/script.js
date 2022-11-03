e81d36e0f724aa09c0267acc1cab2253c143919b
Paul Iverson Cortez <pauliversonc@gmail.com>
2022-11-03 10:12:29 +0800

    add mobile responsive and sticky navbar

--- js/script.js
+++ js/script.js
@@ -284,8 +284,38 @@ document.getElementById("details").onchange = function(){
 };
 
 
+// Mobile nav toggle
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".section-header");
 
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  
+});
 
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
 
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 
